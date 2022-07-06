332fd6b2db6b326ce04382c140ab14b8a6d0e712
soupa1 <98047746+soupa1@users.noreply.github.com>
2022-07-06 14:38:07 -0300

    06 07 22

--- sketch.js
+++ sketch.js
@@ -12,30 +12,62 @@ let world;
 
 var chao;
 var rope;
+var fruta;
+var conexao;
+var backgroundImg, watermelon, coelho;
+var coelhoSpr;
+var botao;
 
-function setup() 
-{
+function preload()  {
+  backgroundImg = loadImage("background.png");
+  watermelon = loadImage("melon.png");
+  coelho = loadImage("Rabbit-01.png");
+}
+
+function setup() {
   createCanvas(500,700);
   engine = Engine.create();
   world = engine.world;
  
   rectMode(CENTER);
   ellipseMode(RADIUS);
+  imageMode(CENTER);
   textSize(50);
 
+  coelhoSpr = createSprite(250, 590, 100, 100);
+  coelhoSpr.addImage(coelho);
+  coelhoSpr.scale = 0.3;
+
+  botao = createImg("cut_button.png");
+  botao.position(220, 30);
+  botao.size(50, 50);
+  botao.mouseClicked(Break);
+
   chao = new Chao(200,690,600,20);
-  rope = new Rope(6,{x: 245, y: 30})
+  rope = new Rope(6,{x: 245, y: 30});
+
+  fruta = Bodies.circle(300,300,15);
+  Matter.Composite.add(rope.body, fruta);
+
+  conexao = new Conexao(rope, fruta);
 }
 
-function draw() 
-{
+function draw() {
   background(51);
+  image(backgroundImg, width/2, height/2, 500, 700);
+
   Engine.update(engine);
    
-  chao.draw()
-  rope.show()
+  chao.draw();
+  rope.show();
+  image(watermelon,fruta.position.x, fruta.position.y, 65, 65);
+
+  drawSprites();
 }
 
 
-
-
+function Break() {
+  conexao.break();
+  rope.break();
+  conexao = null;
+}
