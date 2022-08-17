5ae5f6686086b46db0b5e2fadccef75650e1bf08
soupa1 <98047746+soupa1@users.noreply.github.com>
2022-08-17 14:38:31 -0300

    17 08 22

--- sketch.js
+++ sketch.js
@@ -17,11 +17,26 @@ var conexao;
 var backgroundImg, watermelon, coelho;
 var coelhoSpr;
 var botao;
+var blink;
+var eat;
+var sad
 
 function preload()  {
   backgroundImg = loadImage("background.png");
   watermelon = loadImage("melon.png");
   coelho = loadImage("Rabbit-01.png");
+  blink = loadAnimation("blink_1.png","blink_1.png","blink_1.png","blink_2.png","blink_3.png");
+  eat = loadAnimation("eat_0.png","eat_1.png","eat_2.png","eat_3.png","eat_4.png");
+  sad = loadAnimation("sad_1.png","sad_2.png", "sad_3.png");
+
+  blink.playing = true;
+  eat.playing = true;
+  sad.playing = true;
+
+
+  blink.looping = true;
+  eat.looping = false;
+  sad.looping = false;
 }
 
 function setup() {
@@ -34,9 +49,16 @@ function setup() {
   imageMode(CENTER);
   textSize(50);
 
+  blink.frameDelay = 4;
+  eat.frameDelay = 10;
+
   coelhoSpr = createSprite(250, 590, 100, 100);
   coelhoSpr.addImage(coelho);
   coelhoSpr.scale = 0.3;
+  coelhoSpr.addAnimation("piscando", blink);
+  coelhoSpr.addAnimation("comendo", eat);
+  coelhoSpr.addAnimation("triste", sad);
+  coelhoSpr.changeAnimation("piscando");
 
   botao = createImg("cut_button.png");
   botao.position(220, 30);
@@ -60,9 +82,20 @@ function draw() {
    
   chao.draw();
   rope.show();
-  image(watermelon,fruta.position.x, fruta.position.y, 65, 65);
 
   drawSprites();
+
+  if (fruta !== null) {
+    image(watermelon,fruta.position.x, fruta.position.y, 65, 65);
+  }
+
+  if (Collision(coelhoSpr, fruta) === true) {
+    coelhoSpr.changeAnimation("comendo");
+  }
+
+  if (fruta !== null && fruta.position.y >= 650) {
+    coelhoSpr.changeAnimation("triste");
+  }
 }
 
 
@@ -70,4 +103,21 @@ function Break() {
   conexao.break();
   rope.break();
   conexao = null;
+}
+
+function Collision(coelho, corpo) {
+  if (corpo !== null) {
+    var distancia = dist(coelho.position.x, coelho.position.y, corpo.position.x, corpo.position.y);
+
+    if (distancia <= 80) {
+      World.remove(engine.world, fruta);
+      fruta = null;
+      return true;
+    }
+    else { 
+      return false;
+    }
+  }
+
+
 }
